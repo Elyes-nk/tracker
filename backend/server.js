2c2b11975a4cfaf3ec2b6b4489725b377f36b067
Elyes-nk <elyesnk@gmail.com>
2020-12-12 21:42:48 +0100

    routes

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,8 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri= process.env.ATLAS_URI;
+//const uri= process.env.ATLAS_URI;
+const uri= 'mongodb://admin:admin@cluster0-shard-00-00.gpfyh.mongodb.net:27017,cluster0-shard-00-01.gpfyh.mongodb.net:27017,cluster0-shard-00-02.gpfyh.mongodb.net:27017/test?ssl=true&replicaSet=atlas-kqxgfv-shard-0&authSource=admin&retryWrites=true&w=majority'
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true});
 const connection = mongoose.connection;
 connection.once('open', () => {
@@ -18,6 +19,11 @@ connection.once('open', () => {
 })
 .catch(() => console.log(err));
 
+//ROUTES
+const exercisesRouter = require('./routes/exercises');
+const usersRouter = require('./routes/users');
+app.use('/exercises', exercisesRouter);
+app.use('/users', usersRouter);
 
 app.listen(port, () => {
     console.log('Here we go ! server is running on port: ${port}')
